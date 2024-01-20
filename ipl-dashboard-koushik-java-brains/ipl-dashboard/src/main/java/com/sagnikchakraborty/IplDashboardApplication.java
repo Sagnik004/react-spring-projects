26719b8e76b3109e76897bb3a3f60fe0fee37211
Sagnik004 <sagnikchakraborty910@yahoo.com>
2024-01-20 19:39:06 +0530

    ipl-dashboard project bootstrapped

---
+++ ipl-dashboard-koushik-java-brains/ipl-dashboard/src/main/java/com/sagnikchakraborty/IplDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.sagnikchakraborty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboardApplication.class, args);
+	}
+
+}
